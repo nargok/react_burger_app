eb6ecd7a6dd92d03843dcb41ead3a8d33e704490
nargok <nargok@gmail.com>
2019-12-22 12:32:26 +0900

    implement BugerBuilder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Layout from './components/Layout/Layout';
+import BugerBulder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <div>
       <Layout>
-        <p>Test</p>
+        <BugerBulder />
       </Layout>
     </div>
   );
