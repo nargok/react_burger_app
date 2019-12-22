9124d94666ad05136aea36e827370fd937f8886b
nargok <nargok@gmail.com>
2019-12-22 12:41:09 +0900

    css module for BurgerBuilder

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
-import classes from './Layout.css';
+import styles from './Layout.module.css';
 
 const layout = ( props ) => (
   <Aux>
     <div>Toolbar , SideDrawer, Backdrop</div>
-    <main className={classes.Content}>
+    <main className={styles.Content}>
       {props.children}
     </main>
   </Aux>
